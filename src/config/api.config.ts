da32829eeb392e42d64ce75102efc55d64feab22
Felipe Miranda <feguido1@gmail.com>
2019-09-13 16:30:09 -0300

    Bugfix - atualizacao da imagem de perfil

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
-    baseUrl: "https://mirandox-curso-spring-ionic.herokuapp.com/",
+    baseUrl: "https://mirandox-curso-spring-ionic.herokuapp.com",
     bucketBaseUrl: "https://mirandox-curso-spring-ionic.s3-sa-east-1.amazonaws.com"
 }
